5ab79da1a475194cc0193e170a691b34029e6dec
bstar042005 <bhavya.16419@gmail.com>
2026-06-04 14:05:39 +0530

    Backend verified

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI);
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("✅ MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.error("❌ MongoDB Connection Failed:");
+    console.error(error);
+
     process.exit(1);
   }
 };
