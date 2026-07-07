450c99f02fb65d0417603c98f9f4d6b7b77b08fe
reocodes-51 <pinsubanna5@gmail.com>
2026-07-07 21:28:50 +0530

    updated api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stockpilot-and-inventory-locator-sq.vercel.app/api"
+  baseURL: "https://stockpilot-and-inventory-locator.onrender.com/api",
 });
 
 export default API;
